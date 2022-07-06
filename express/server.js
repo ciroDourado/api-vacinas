bc4fd02fab1c60c4fd374c565fa90c4f189c5a2a
ciroDourado <ciro.brz@gmail.com>
2022-07-06 20:51:41 -0300

    feature: inclui rotas para serem abertas

--- express/server.js
+++ express/server.js
@@ -19,7 +19,9 @@ app.use(url_encoded_parser)
 
 // rotas
 require("../express/routes/home.js")(app)
+require("../express/routes/vacinados/routes.js")(app)
 require("../express/routes/vacinas/routes.js")(app)
+require("../express/routes/validacao/routes.js")(app)
 
 let servidor = app.listen(env.APPLICATION_PORT)
 
