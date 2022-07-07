8deb46aef5f73919b33fabb3b3fd97c0d914360e
ciroDourado <ciro.brz@gmail.com>
2022-07-07 15:01:29 -0300

    (wip) interliga servico de vacinas nas controllers

--- express/controllers/vacinas/controller.js
+++ express/controllers/vacinas/controller.js
@@ -1,22 +1,22 @@
-// const { PrismaClient } = require("@prisma/client")
-// const   repositorio    = new PrismaClient()
+const servicoDeCadastro = require("../../../vacinas/cadastro/main.js"); 
+const servicoDeListagem = require("../../../vacinas/listagem/main.js"); 
 
 
-// exports.criar = (req, res) => {
+exports.criar = async (req, res) => {
+  let formulario = req.body;
+  let resultado = await servicoDeCadastro.cadastrar(formulario);
+  return res.json(resultado ?? "erro");
+};
+exports.listar = async (req, res) => {
+  let vacinas = await servicoDeListagem.listar();
+  return res.json(vacinas);
+};
+exports.encontrar = (req, res) => {
 
-// };
-// exports.listar = async (req, res) => {
-//   let vacinas = await repositorio
-//     .vacina
-//     .findMany()
-//   return res.json({vacinas: vacinas})
-// };
-// exports.encontrar = (req, res) => {
+};
+exports.atualizar = (req, res) => {
 
-// };
-// exports.atualizar = (req, res) => {
+};
+exports.deletar = (req, res) => {
 
-// };
-// exports.deletar = (req, res) => {
-
-// };
+};
