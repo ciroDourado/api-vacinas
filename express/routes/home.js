9beeba7d7fedfa1cd1e8c23718a18435561e8fab
ciroDourado <ciro.brz@gmail.com>
2022-07-07 14:59:44 -0300

    (wip) lista rotas de vacinas na home

--- express/routes/home.js
+++ express/routes/home.js
@@ -4,19 +4,13 @@ module.exports = app => {
   router.get("/", (req, res) => {
     let urls = {
       urls: {
-        // vacinas: [
-        //   { url: "/vacinas", método: "GET", descrição: "Listagem de vacinas" },
-        //   { url: "/vacinas", método: "POST", descrição: "Cadastro de vacinas" },
-        //   { url: "/vacinas/:id", método: "GET", descrição: "Busca de vacina" },
-        //   { url: "/vacinas/:id", método: "PUT", descrição: "Atualização de vacina" },
-        //   { url: "/vacinas/:id", método: "DELETE", descrição: "Deletar vacina" }
-        // ],
+        vacinas: [
+          { url: "/vacinas", método: "GET", descrição: "Listagem de vacinas" },
+          { url: "/vacinas", método: "POST", descrição: "Cadastro de vacinas" },
+        ],
         vacinados: [
           { url: "/vacinados", método: "GET", descrição: "Listagem de vacinados" },
           { url: "/vacinados", método: "POST", descrição: "Cadastro de vacinados" },
-          // { url: "/vacinados/:id", método: "GET", descrição: "Busca de vacinado" },
-          // { url: "/vacinados/:id", método: "PUT", descrição: "Atualização de vacinado" },
-          // { url: "/vacinados/:id", método: "DELETE", descrição: "Deletar vacinado" }
         ],
         enderecos: [
           { url: "/enderecos", método: "GET", descrição: "Listagem de endereços" },
