2fecee25989d94c1de77c6bf676869b0636e1f32
ciroDourado <ciro.brz@gmail.com>
2022-07-07 10:46:30 -0300

    (wip) refactor: deleta arquivos pois serao movidos

--- listagem/vacinados/main.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require("@prisma/client")
-const   repositorio    = new PrismaClient()
-
-exports.listar = async function ()
-{
-  return await repositorio
-    .vacinado
-    .findMany();
-}
