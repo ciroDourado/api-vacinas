cfb6ab2625ecc01e465edb9ff13464611ecb09f8
ciroDourado <ciro.brz@gmail.com>
2022-07-07 14:36:14 -0300

    (wip) retorna que id é sempre válido

--- enderecos/validacao/morador.js
+++ enderecos/validacao/morador.js
@@ -4,5 +4,5 @@ exports.idEValido = async function(id) {
   // let vacinadoExiste            = await servicoBusca.busca(id) != null;
   // let vacinadoNaoPossuiEndereco = await servicoBusca.possuiEndereco(id) == false;
   // return vacinadoExiste && vacinadoNaoPossuiEndereco;
-  return false;
+  return true;
 }
