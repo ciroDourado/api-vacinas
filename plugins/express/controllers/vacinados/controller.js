a3320884fa65296ad3acb83a953598476a13e51d
ciroDourado <ciro.brz@gmail.com>
2022-07-10 03:11:59 -0300

    refactor: altera resposta padrão de hardcoded para resposta vinda do módulo de Respostas

--- plugins/express/controllers/vacinados/controller.js
+++ plugins/express/controllers/vacinados/controller.js
@@ -5,8 +5,8 @@ const servicoDeListagem = require("../../../../vacinados/listagem/main.js");
 
 exports.criar = async (req, res) => {
   let formulario = req.body;
-  let resultado  = await servicoDeCadastro.cadastrar(formulario);
-  return res.json(resultado ?? "erro");
+  let resultado = await servicoDeCadastro.cadastrar(formulario);
+  return res.json(resultado);
 };
 exports.listar = async (req, res) => {
   let vacinados = await servicoDeListagem.listar();
