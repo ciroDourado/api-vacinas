fa68aaa5465a3720719c1a7143094d26ed27f3c0
ciroDourado <ciro.brz@gmail.com>
2022-07-12 17:37:47 -0300

    fix: resolve conflitos

--- plugins/express/routes/home.js
+++ plugins/express/routes/home.js
@@ -4,22 +4,9 @@ module.exports = app => {
   router.get("/", (req, res) => {
     let urls = {
       urls: {
-<<<<<<< HEAD
-        aplicacao_de_vacinas: [
-          { url: "/aplicacao_de_vacinas", método: "POST", descrição: "Cadastrar aplicação de vacinas" },
-        ],
-=======
->>>>>>> main
         doses: [
           { url: "/doses", método: "GET", descrição: "Listagem de doses" },
           { url: "/doses", método: "POST", descrição: "Cadastro de doses" },
-        ],
-        doses_das_vacinas: [
-          { url: "/doses_das_vacinas", método: "GET", descrição: "Listagem de doses das vacinas" },
-        ],
-        enderecos: [
-          { url: "/enderecos", método: "GET", descrição: "Listagem de endereços" },
-          { url: "/enderecos", método: "POST", descrição: "Cadastro de endereços" },
         ],
         vacinados: [
           { url: "/vacinados", método: "GET", descrição: "Listagem de vacinados" },
@@ -28,6 +15,12 @@ module.exports = app => {
         vacinas: [
           { url: "/vacinas", método: "GET", descrição: "Listagem de vacinas" },
           { url: "/vacinas", método: "POST", descrição: "Cadastro de vacinas" },
+        ],
+        doses_das_vacinas: [
+          { url: "/doses_das_vacinas", método: "GET", descrição: "Listagem de doses das vacinas" },
+        ],
+        aplicacao_de_vacinas: [
+          { url: "/aplicacao_de_vacinas", método: "POST", descrição: "Cadastrar aplicação de vacinas" },
         ],
       }
     }
